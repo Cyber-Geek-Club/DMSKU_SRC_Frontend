eeee979c3783ddf91e78f11731df14e35f0b225b
PPEACH21 <sohizz1201@gmail.com>
2025-08-20 13:39:17 +0700

    feat:Create Document PAGE

--- src/Tempdata.ts
+++ src/Tempdata.ts
@@ -5,6 +5,10 @@ export interface DocumentStatus{
   waituser: string[];
 }
 
+export interface FileDoc{
+  Userid: string[];
+}
+
 export const temp_data: DocumentStatus[] =[
   {
     Userid: "00001",
@@ -17,6 +21,16 @@ export const temp_data: DocumentStatus[] =[
     documentname: "โครงการDSMKU1",
     status: 0,
     waituser: ["02", "03", "04", "05","09", "08",]
+  }
+];
+
+
+export const file: FileDoc[] =[
+  {
+    Userid: ["00077","0078","0079","0080"],
+  },
+  {
+    Userid: ["000078"],
   }
 ];
 
